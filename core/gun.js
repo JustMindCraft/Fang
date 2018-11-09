38839a8b418facf5df233016ab0a345ad5287b4a
simon <xsqfeather@gmail.com>
2018-11-09 13:30:20 +0800

    https gun suport

--- core/gun.js
+++ core/gun.js
@@ -1,4 +1,4 @@
 export const  Gun = require('gun');
 require('gun/sea');
 const sea = Gun.SEA
-export const gun =  Gun('http://localhost:8000/gun');
+export const gun =  Gun('https://zhengjue.lododor.com/gun');
